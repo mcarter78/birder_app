1f2a35c67b0e492ff7870ee806fee93826033031
mcarter78 <sjsharksftw@gmail.com>
2016-04-02 14:51:49 -0700

    adds layout route

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     router = express.Router(),
+    path = require('path'),
     usersController = require('../controllers/usersController'),
     birdsController = require('../controllers/birdsController'),
     entriesController = require('../controllers/entriesController');
@@ -28,5 +29,10 @@ router.route('/api/users/:id/entries')
 router.route('/api/users/:id/entries/:id')
   .patch(entriesController.update) // WORKING
   .delete(entriesController.delete); // WORKING
+
+router.route('*')
+  .get(function(req, res) {
+    res.sendFile(path.join(__dirname + '/../../views/index.html'));
+  });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 app.use(logger('dev'));
 app.use(routes);
 
