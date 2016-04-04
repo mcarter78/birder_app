0c87982f28bc514b4dbc673d09fbe45bd6d46a2f
mcarter78 <sjsharksftw@gmail.com>
2016-04-04 12:52:42 -0700

    completes angular refactor

--- public/js/app.js
+++ public/js/app.js
@@ -9,12 +9,32 @@ function birdyConfig($stateProvider, $locationProvider) {
             url: '/',
             templateUrl: '/templates/home.html'
           })
+          .state('birds-index', {
+            url: '/birds',
+            controller: 'BirdsController',
+            controllerAs: 'birds',
+            templateUrl: '/templates/birds-index.html'
+          });
+
+          $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+          });
 }
 
-function BirdsController(){
-
+function BirdsController($http){
+  var vm = this;
+  vm.allBirds = [];
+  vm.getAll = function() {
+    $http.get('/api/birds')
+      .then(function(res) {
+        vm.allBirds = res.data;
+      });
+  };
+  vm.getAll();
+  console.log(vm.allBirds);
 }
 
 function EntriesController(){
-  
+
 }
