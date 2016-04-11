af82d812d6b4edd5147c967fd9b069ead97cc0b9
mcarter78 <sjsharksftw@gmail.com>
2016-04-11 12:04:39 -0700

    adds Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: /bin/bash -c '(nohup ssh -n -i compose-heroku-key -o StrictHostKeyChecking=no \
+-N compose@dblayer.com -p 1000 -L 127.0.0.1:28015:10.11.12.2:28015 &);
--- api/db/config.js
+++ api/db/config.js
@@ -1,7 +1,7 @@
 var thinky = require('thinky')({
-  host: 'rethinkdb://aws-us-east-1-portal.12.dblayer.com',
-  port: '11113',
-  authKey: 'Gg5HfgrK3TnhljtGeqPQ9HH17kAjiEAGjbYZJvhL50',
+  host: 'localhost',
+  port: '28015',
+  //authKey: 'Gg5HfgrK3TnhljtGeqPQ9HH17kAjiEAGjbYZJvhL50',
   db: 'birder_app'
 });
 
