1b9493b89d39da0daff708283576a0a1ef65a1ad
mcarter78 <sjsharksftw@gmail.com>
2016-04-11 10:39:00 -0700

    trying again to deploy db

--- api/db/config.js
+++ api/db/config.js
@@ -1,6 +1,6 @@
 var thinky = require('thinky')({
-  host: 'aws-us-east-1-portal.13.dblayer.com',
-  port: '28015',
+  host: 'rethinkdb://aws-us-east-1-portal.12.dblayer.com',
+  port: '11113',
   authKey: 'Gg5HfgrK3TnhljtGeqPQ9HH17kAjiEAGjbYZJvhL50',
   db: 'birder_app'
 });
