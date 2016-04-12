495647c1c0e52a562c95181ab2140973d88586bc
mcarter78 <sjsharksftw@gmail.com>
2016-04-12 12:26:42 -0700

    deploying?

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python tunnel.py
+
--- api/db/config.js
+++ api/db/config.js
@@ -1,8 +1,26 @@
 var thinky = require('thinky')({
-  host: 'localhost',
-  port: '28015',
+  host: 'aws-us-east-1-portal.12.dblayer.com',
+  port: '11113',
   authKey: 'Gg5HfgrK3TnhljtGeqPQ9HH17kAjiEAGjbYZJvhL50',
   db: 'birder_app'
+});
+
+var r = thinky.r;
+var fs = require('fs');
+
+fs.readFile('./cacert', function(err, caCert) {
+  r.connect({
+    host: 'aws-us-east-1-portal.12.dblayer.com',
+    port: 11113,
+    authKey: 'Gg5HfgrK3TnhljtGeqPQ9HH17kAjiEAGjbYZJvhL50',
+    ssl: {
+      ca: caCert
+    }
+  }, function(error, conn) {
+    r.dbList().run(conn, function(err, results) {
+      console.log(results);
+    });
+  });
 });
 
 module.exports = thinky;
